02109d591c99681d75d01e902d45aff70e6d5901
javier rojas <javie.rojasc@duocuc.cl>
2024-07-03 09:56:14 -0400

    inicio echo

---
+++ funciones.py
@@ -0,0 +1,6 @@
+import prueba
+
+nombre= []
+rut 
+
+
---
+++ prueba.py
@@ -0,0 +1,22 @@
+
+import funciones
+
+print("menu")
+print("""1. Registrar pedido.
+         2. Listar todos los pedidos.
+         3. Buscar pedido por RUT.
+         4. Imprimir hoja de ruta.
+         5. Salir del programa.""")
+opc = int(input("que opcion desea usar"))
+if opc == 1 :
+    pass
+elif opc == 2:
+    pass
+elif opc == 3:
+    pass
+elif opc ==4:
+    pass
+elif opc == 5:
+    pass
+else:
+    print ("opcion no existe ")

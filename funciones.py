824cbb64264490b755c1ea02fd83f2f7a8f4dfe3
javier rojas <javie.rojasc@duocuc.cl>
2024-07-03 11:12:53 -0400

    arreglar problemas

--- funciones.py
+++ funciones.py
@@ -12,35 +12,36 @@ cili_15=[]
 venta=[]
 
 def opc_1():
-    nom = input("ingrese su nombre ")
+    nom = input("ingrese su nombre: ")
     nombre.append(nom)
-    ru = int(input("ingrese su rut sin puntos ni -"))
+    ru = int(input("ingrese su rut sin puntos ni -:"))
     rut.append(ru)
-    dire = input("ingrese su direccion")
+    dire = input("ingrese su direccion:")
     direccion.append(dire)
-    comu = input("ingrese la comuna")
+    comu = input("ingrese la comuna:")
     comuna.append(comu)
     while True:
         print("""que desea perdir
-                 1.cilindro de 5kg
-                 2.cilindro de 15kg
-                 3.salir""")
-        opcionci= int(input)
+    1.cilindro de 5kg
+    2.cilindro de 15kg
+    3.salir""")
+        opcionci= int(input("que galon desea pedir:"))
         if opcionci ==1:
-            cantidad = int(input("cuantos desea pedir"))
+            cantidad = int(input("cuantos desea pedir:"))
             cili_5.append(cantidad)
             acumulador=cantidad * cilindro5
         elif opcionci==2: 
-            cantidad = int(input("cuantos desea pedir"))
+            cantidad = int(input("cuantos desea pedir:"))
             cili_15.append(cantidad)
             acumulador=cantidad * cilindro15
         elif opcionci==3:
             venta.append(acumulador)
+            acumulador = acumulador*0
             print("pedido con exito")
-            time.sleep(3)
             break
+    return
 def opc_2():
-    if acumulador == 0:
+    if not venta:
         print("error no hay ningun perdido")
     else:
         print(nombre)
@@ -51,7 +52,7 @@ def opc_2():
         print(cili_15)
         print(venta)
 def opc_3():
-    buscar = int(input("ingrese el rut que esta buscando"))
+    buscar = int(input("ingrese el rut que esta buscando:"))
     rut.index(buscar)
     print(nombre)
     print(rut)
@@ -62,22 +63,24 @@ def opc_3():
     print(venta)
 
 def opc_4():
-    if not acumulador:
+    if not venta:
         print("no hay compras")
     else:
         import datetime,csv
+        nombre_archivo =("que nombre quiere dar el archivo:")
         nombre_archivo= str(datetime.datetime.now()).replace("."," ").replace("."," ").replace("+"," ")
         with open (nombre_archivo,"w",newline="")as file :
             escritor = csv.writer(file)
             escritor .writerows(venta)
             
 def opc_5():
-    if acumulador== 0:
+    if not venta:
         print("adios")
-        time.sleep
-
-    print ("su pedido fue realisado adios")
-    time.sleep(3)
-    exit
+        time.sleep(3)
+        exit
+    else:
+        print ("su pedido fue realisado adios")
+        time.sleep(3)
+        exit
 
 
--- prueba.py
+++ prueba.py
@@ -1,21 +1,22 @@
 from funciones import*
 import time,csv
-print(      "menu")
-print("""1. Registrar pedido.
-         2. Listar todos los pedidos.
-         3. Buscar pedido por RUT.
-         4. Imprimir hoja de ruta.
-         5. Salir del programa.""")
-opc = int(input("que opcion desea usar:"))
-if opc == 1 :
-    opc_1()
-elif opc == 2:
-    opc_2()
-elif opc == 3:
-    opc_3()
-elif opc ==4:
-    opc_4()
-elif opc == 5:
-    opc_5()   
-else:
-    print ("opcion no existe ")
+while True:
+    print("""menu
+    1. Registrar pedido.
+    2. Listar todos los pedidos.
+    3. Buscar pedido por RUT.
+    4. Imprimir hoja de ruta.
+    5. Salir del programa.""")
+    opc = int(input("que opcion desea usar:"))
+    if opc == 1 :
+        opc_1()
+    elif opc == 2:
+        opc_2()
+    elif opc == 3:
+        opc_3()
+    elif opc ==4:
+        opc_4()
+    elif opc == 5:
+        opc_5()   
+    else:
+        print ("opcion no existe ")
